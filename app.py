94581ccd8b2aa23785dddf461cc95c33134b77b3
lullyandrose <artificialflowersgc@gmail.com>
2024-12-04 00:15:11 +0800

    Updated app.py to bind to dynamic PORT

--- app.py
+++ app.py
@@ -218,6 +218,6 @@ def summary():
 
 
 
-if __name__ == '__main__':
-    # Set the port to the value in the environment variable 'PORT', defaulting to 5000 if not specified
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
